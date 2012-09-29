f3c4fe311a2aeead95e9839a29f6c469901bbe8d
Aurelijus Banelis <aurelijus@banelis.lt>
2012-09-29 23:56:09 +0300

    Camera pointer.

--- src/main.c
+++ src/main.c
@@ -10,40 +10,80 @@
  */
 
 #define ESCAPE 27
+#define CAMERA_WIDTH 2
+#define CAMERA_HEIGHT 2
 
+/*
+ * Global variables
+ */
+
+float pointerX = 0;
+float pointerY = 0;
+int windowWidth = 500;
+int windowHeight = 500;
+
+/*
+ * Transformations
+ */
+
+void saveWindowSize(int width, int height) {
+    glViewport(0, 0, width, height);
+    windowWidth = width;
+    windowHeight = height;
+    glutPostRedisplay();
+}
+
+void to3dCoordinates(int windowX, int windowY, float* x, float* y) {
+    *x = windowX / (float) windowWidth * CAMERA_WIDTH;
+    *y = windowY / (float) windowHeight * CAMERA_HEIGHT;
+}
 
 /*
  * Render
  */
 
-
 void renderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+//        // Camera as window
+//        glLoadIdentity();               
+//        glRotatef(180, 1, 0, 0); 
+//        glTranslatef(-1, -1, 0);
+        
         // X
         glColor3f(1,0,0);
 	glBegin(GL_TRIANGLES);
-                glVertex3f(1.0,0.0,0.0);
-		glVertex3f(0.0,-0.3,0.0);
-		glVertex3f(0.0,0.3,0.0);
-        glEnd();
+            glVertex3f(1.0,0.0,0.0);
+            glVertex3f(0.0,-0.3,0.0);
+            glVertex3f(0.0,0.3,0.0);
+	glEnd();
         
         // y
         glColor3f(0,1,0);
 	glBegin(GL_TRIANGLES);
-                glVertex3f(0.0,1.0,0.0);
-		glVertex3f(-0.3,0.0,0.0);
-		glVertex3f(0.3,0.0,0.0);
+            glVertex3f(0.0,1.0,0.0);
+            glVertex3f(-0.3,0.0,0.0);
+            glVertex3f(0.3,0.0,0.0);
         glEnd();
         
         // z
         glColor3f(0,0,1);
 	glBegin(GL_TRIANGLES);
-                glVertex3f(0.0,0.0,1.0);
-		glVertex3f(-0.3,0.0,0.0);
-		glVertex3f(0.3,0.0,0.0);
+            glVertex3f(0.0,0.0,1.0);
+            glVertex3f(-0.3,0.0,0.0);
+            glVertex3f(0.3,0.0,0.0);
         glEnd();
         
+        // Pointer
+        glColor3f(0,1,1);
+        glBegin(GL_QUADS);
+            float size = 0.05;
+            glVertex2f(pointerX-size,pointerY-size);
+            glVertex2f(pointerX-size,pointerY+size);
+            glVertex2f(pointerX+size,pointerY+size);
+            glVertex2f(pointerX+size,pointerY-size);
+        glEnd();
+
         glutSwapBuffers();
 }
 
@@ -57,7 +97,9 @@ void mouseClick(int button, int state, int x, int y) {
 }
 
 void mouseMove(int x, int y) {
-    printf("Move (%dx%d)\n", x, y);
+    to3dCoordinates(x, y, &pointerX, &pointerY);
+    glutPostRedisplay();
+    printf("Move (%dx%d)=(%f %f)\n", x, y, pointerX, pointerY);
 }
 
 void mouseDrag(int x, int y) {
@@ -73,7 +115,7 @@ void keyPressed(unsigned char key, int x, int y) {
     printf("Key %c (%dx%d)\n", key, x, y);
     switch(key) {
         case ESCAPE:
-            exit(0); //glutLeaveMainLoop();
+        exit(0); //glutLeaveMainLoop();
         break;
     }
     glutPostRedisplay();
@@ -108,28 +150,30 @@ void init() {
     // Keyboard
     glutKeyboardFunc(keyPressed);
     
+    // Window
+    glutReshapeFunc(saveWindowSize);
+    
     // Camera
     gluLookAt ( 1, 1, 0,    //   y|/      y| /  /
                 1, 1, 1,    // ---/---x    |/  V
                 0,-1, 0 );  //  z/|       z/-----x
-    
 }
 
 int main(int argc, char **argv) {
 
-    // init GLUT and create Window
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE);
-    glutInitWindowPosition(1000,500);
-    glutInitWindowSize(320,320);
-    glutCreateWindow("U1");
+	// init GLUT and create Window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_ALPHA);
+	glutInitWindowPosition(1000,500);
+	glutInitWindowSize(320,320);
+	glutCreateWindow("U1");
     init();
 
-    // register callbacks
-    glutDisplayFunc(renderScene);
+	// register callbacks
+	glutDisplayFunc(renderScene);
 
-    // enter GLUT event processing cycle
-    glutMainLoop();
-
-    return 1;
+	// enter GLUT event processing cycle
+	glutMainLoop();
+	
+	return 1;
 }
